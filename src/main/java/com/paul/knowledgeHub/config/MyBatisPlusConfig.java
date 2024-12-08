01bf591b41b241d294b5336e16e7844a13bd236c
paul121388 <2265370493@qq.com>
2024-12-08 14:28:06 +0800

    引入Redisson配置，编写Redisson配置类，设置属性配置，移除多余的文件

--- src/main/java/com/paul/knowledgeHub/config/MyBatisPlusConfig.java
+++ src/main/java/com/paul/knowledgeHub/config/MyBatisPlusConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * MyBatis Plus 配置
- *
- * @author https://github.com/lipaul
  */
 @Configuration
 @MapperScan("com.paul.knowledgeHub.mapper")
